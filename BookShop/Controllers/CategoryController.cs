2e0abd1a0f4dc880a71d5635144a1a23b05c1e81
huylebron <doquochuy27102003@gmail.com>
2024-08-18 15:43:05 +0700

    CRUD categories complete

--- BookShop/Controllers/CategoryController.cs
+++ BookShop/Controllers/CategoryController.cs
@@ -7,36 +7,126 @@ namespace BookShop . Controllers
 {
     public class CategoryController : Controller
     {
-        private readonly ApplicationDbContext _db ;s
+        private readonly ApplicationDbContext _db ;
 
-        public CategoryController ( ApplicationDbContext db )
-        { this . _db = db ; }
+        public CategoryController ( ApplicationDbContext db ) {
+            this . _db = db ;
+        }
 
-        public IActionResult Index ( )
-        { IEnumerable < Category > objCategoryList = _db . Categories . ToList ( ) ;
-          return View ( objCategoryList ) ; }
+        public IActionResult Index ( ) {
+            IEnumerable < Category > objCategoryList = _db . Categories . ToList ( ) ;
+            return View ( objCategoryList ) ;
+        }
 
-        public IActionResult Create ( )
-        { return View ( ) ; }
+        public IActionResult Create ( ) {
+            return View ( ) ;
+        }
 
         //post
         [ HttpPost ] // counter faken ;
         [ ValidateAntiForgeryToken ] //  counter noob hacker :)))
 
         // auto post category 
-        public IActionResult Create ( Category obj )
-        { if ( obj . Name == obj . DisplayOrder . ToString ( ) )
-          {
-              ModelState . AddModelError ( "CustomError" , "the Name must not same  " ) ;
-          }
+        public IActionResult Create ( Category obj ) {
+            if ( obj . Name == obj . DisplayOrder . ToString ( ) )
+            {
+                ModelState . AddModelError ( "CustomError" , "the Name must not same  " ) ;
+            }
 
-          if ( ModelState . IsValid )
-          {
-              _db . Categories . Add ( obj ) ;
-              _db . SaveChanges ( ) ;
-              return RedirectToAction ( "index" ) ;
-          }
+            if ( ModelState . IsValid )
+            {
+                _db . Categories . Add ( obj ) ;
+                _db . SaveChanges ( ) ;
+                return RedirectToAction ( "index" ) ;
+            }
 
-          return View ( obj ) ; }
+            return View ( obj ) ;
+        }
+
+        public IActionResult Edit ( int ? id ) {
+            if ( id == null || id == 0 )
+            {
+                return NotFound ( ) ;
+            }
+
+            var categoryfromDb = _db . Categories . Find ( id ) ;
+            var categoryfromDbfirst = _db . Categories . FirstOrDefault ( u => u . Id == id ) ;
+            var categoryfromDbsingle = _db . Categories . SingleOrDefault ( u => u . Id == id ) ;
+
+            if ( categoryfromDb == null )
+            {
+                return NotFound ( ) ;
+            }
+
+
+            return View ( categoryfromDb ) ;
+        }
+
+        //post
+        [ HttpPost ] // counter faken ;
+        [ ValidateAntiForgeryToken ] //  counter noob hacker :)))
+
+        // auto post category 
+        public IActionResult Edit ( Category obj ) {
+            if ( obj . Name == obj . DisplayOrder . ToString ( ) )
+            {
+                ModelState . AddModelError ( "CustomError" , "the Name must not same  " ) ;
+            }
+
+            if ( ModelState . IsValid )
+            {
+                _db . Categories . Update ( obj ) ;
+                _db . SaveChanges ( ) ;
+                return RedirectToAction ( "index" ) ;
+            }
+
+
+            return View ( obj ) ;
+        }
+
+
+        public IActionResult Delete ( int ? id ) {
+            if ( id == null || id == 0 )
+            {
+                return NotFound ( ) ;
+            }
+
+            var categoryfromDb = _db . Categories . Find ( id ) ;
+            //var categoryfromDbfirst = _db.Categories.FirstOrDefault(u => u.Id == id);
+            //  var categoryfromDbsingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+
+            if ( categoryfromDb == null )
+            {
+                return NotFound ( ) ;
+            }
+
+
+            return View ( categoryfromDb ) ;
+        }
+
+        //post
+        [ HttpPost ] // counter faken ;
+        [ ValidateAntiForgeryToken ] //  counter noob hacker :)))
+
+        // auto post category 
+        public IActionResult DeletePost ( int ? id ) {
+            var obj = _db . Categories . Find ( id ) ;
+            // var categoryfromDbfirst = _db.Categories.FirstOrDefault(u => u.Id == id);
+            //var categoryfromDbsingle = _db.Categories.SingleOrDefault(u => u.Id == id);
+
+            if ( obj == null )
+            {
+                return NotFound ( ) ;
+            }
+            else
+            {
+                _db . Categories . Remove ( obj ) ;
+                _db . SaveChanges ( ) ;
+                return RedirectToAction ( "index" ) ;
+            }
+
+
+            return View ( obj ) ;
+        }
     }
 }
--- BookShop/Controllers/HomeController.cs
+++ BookShop/Controllers/HomeController.cs
@@ -1,32 +1,29 @@
-﻿using BookShop.Models;
-using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+﻿using BookShop . Models ;
+using Microsoft . AspNetCore . Mvc ;
+using System . Diagnostics ;
 
-namespace BookShop.Controllers
+namespace BookShop . Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
+        private readonly ILogger < HomeController > _logger ;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
+        public HomeController ( ILogger < HomeController > logger ) {
+            _logger = logger ;
         }
 
-        public IActionResult Index()
-        {
-            return View();
+        public IActionResult Index ( ) {
+            return View ( ) ;
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
+        public IActionResult Privacy ( ) {
+            return View ( ) ;
         }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        [ ResponseCache ( Duration = 0 , Location = ResponseCacheLocation . None , NoStore = true ) ]
+        public IActionResult Error ( ) {
+            return View (
+                new ErrorViewModel { RequestId = Activity . Current ? . Id ?? HttpContext . TraceIdentifier } ) ;
         }
     }
 }
